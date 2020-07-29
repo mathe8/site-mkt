5c9d284864b3df414e411904272b2d6926b47675
LeonardoMuniz <leonardomunizoficial@gmail.com>
2020-07-28 23:06:58 -0300

    editando cabeçalho e colocando thub em bg

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php require_once('header.php'); 
 
-if(have_posts()):
-    while(have_posts()): 
-        the_post();
+// if(have_posts()):
+//     while(have_posts()): 
+//         the_post();
         
-        the_content();
-    endwhile;
-endif;
+//         the_content();
+//     endwhile;
+// endif;
 
 ?>
 
-
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Fluid jumbotron</h1>
+        <p class="lead">pies the entire horizontal space of its parent.</p>
+    </div>
+</div>
 
 
 <div class="container ">
@@ -22,12 +27,11 @@ endif;
             if ( $loop->have_posts() ) { 
                 while ( $loop->have_posts() ) {
                     $loop->the_post();
+                    
         ?>
 
         <div class="clientes-card card col-md-3  " style="margin: 0px 15px 10px 0px;">
-            <figure>
-                <?=the_post_thumbnail(array(253, 189));?>
-            </figure>
+        <?='<div class="card-img-top" style="background: url('.the_post_thumbnail().')"></div>'?>
             <div class="card-body">
                 <h5 class="card-title"><?= the_title(); ?></h5>
                 <p class="card-text"><?= the_content(); ?></p>
@@ -42,5 +46,8 @@ endif;
         ?>
     </div>
 </div>
+
+
+
 
 <?php require_once('footer.php'); ?>
