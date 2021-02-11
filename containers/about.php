320158816c41948bca43f1727552be9832d4e91c
mathe8 <mvieirabezerra@gmail.com>
2021-02-11 16:15:11 -0300

    Renomeando Pastas

---
+++ containers/about.php
@@ -0,0 +1,119 @@
+    <div class="container-fluid mb-3 mt-5">
+        
+        <div class="row" id="id1"><div class="col" ><h2 class="text-center">Clientes</h2></div></div>    
+    
+        <div class="row mb-3 d-flex justify-content-center apresentation">
+        
+                <div class="card hvr-grow hvr-mouse-on" style="width: 22.5rem;">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <h5><img src="https://image.flaticon.com/icons/svg/15/15659.svg" alt=""></h5>
+                            </div>
+                            <div class="col">
+                                <h5 class="card-title">Sobre Nós</h5>
+                                <p class="card-text">Some quick example text <br> quick example. </p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card hvr-grow hvr-mouse-on" style="width: 22.5rem;">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <h5><img src="https://image.flaticon.com/icons/svg/15/15659.svg" alt=""></h5>
+                            </div>
+                            <div class="col">
+                                <h5 class="card-title">Sobre Nós</h5>
+                                <p class="card-text">Some quick example text <br> quick example. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card hvr-grow hvr-mouse-on" style="width: 22.5rem;">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <h5><img src="https://image.flaticon.com/icons/svg/15/15659.svg" alt=""></h5>
+                            </div>
+                            <div class="col">
+                                <h5 class="card-title">Sobre Nós</h5>
+                                <p class="card-text">Some quick example text <br> quick example. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card hvr-grow hvr-mouse-on" style="width: 22.5rem;">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <h5><img src="https://image.flaticon.com/icons/svg/15/15659.svg" alt=""></h5>
+                            </div>
+                            <div class="col">
+                                <h5 class="card-title">Sobre Nós</h5>
+                                <p class="card-text">Some quick example text <br> quick example. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+    </div> 
+
+    <div class="row p-5 mt-5 mb-4 bg-light" id="sobre">
+        <div class="col-md-6">
+            <div class="container">
+                <div class="d-flex flex-row-reverse">
+                    
+                    <img src="https://assets.justinmind.com/wp-content/uploads/2018/11/Lorem-Ipsum-alternatives.png" data-toggle="modal" data-target="#modalCenter" class="img-fluid img-sobre" width="500px" alt="">
+
+                    <div class="modal fade modal-sobre" id="modalCenter" tabindex="-1" role="dialog" aria-labelledby="modalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="https://assets.justinmind.com/wp-content/uploads/2018/11/Lorem-Ipsum-alternatives.png" class="img-fluid img-sobre-modal" alt="">
+                            </div>
+                            <div class="modal-footer justify-content-center">
+                                <h4>Confira as Vangatens</h4>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card border-none bg-light" style="width: 35rem; margin-left: -25px;">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="card-title">Sobre Nós</h2>
+                            <p class="card-text">Some quick example text quick example quick text. 
+                                <br> Some quick example text quick example.
+                                <br> Some quick example text quick.
+                            </p>
+                            <button type="button" data-toggle="modal" data-target="#modalCenter" class="btn btn-outline-primary">Explore Mais</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h4>Some quick example text quick example.</h4>
+            <p>Some quick example text quick example. <br> Some quick example text. <br> Some quick. <br> </p>
+        </div>
+    </div>
+
+</div>
---
+++ containers/navbar.php
@@ -0,0 +1,26 @@
+<div class="container">        
+        <div class="container-fluid">            
+            <nav class="navbar navbar-expand-lg navbar-light py-1 px-md-1 shift">
+                <a class="navbar-brand animate__animated animate__bounce" href=""><img src="http://localhost/wordpress/wp-content/uploads/2020/08/SWL_LOGOMARCA.png" width="250" height="auto" class="d-inline-block align-top navbar-logo" alt="logo" loading="lazy"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto navbar-principal">
+                      <li class="nav-item active">
+                        <a class="nav-link" href="">Home<span class="sr-only">(current)</span></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#" onclick="$('html,body').animate({scrollTop: $('#servicos').offset().top-10},'slow');return false;">Serviços</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#" onclick="$('html,body').animate({scrollTop: $('#sobre').offset().top},'slow');return false;">Sobre</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="" onclick="$('html,body').animate({scrollTop: $('#contato').offset().top},'slow');return false;">Contato</a>
+                      </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
